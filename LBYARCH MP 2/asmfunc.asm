60d1966305bde7413117b535f793e78368ca80b2
Enrique <lejanoenrique@gmail.com>
2024-03-30 19:19:20 +0800

    successfully implemented dapxy() function.

--- LBYARCH MP 2/asmfunc.asm
+++ LBYARCH MP 2/asmfunc.asm
@@ -1,39 +1,50 @@
 section .data
+a dq 0
 vectorsize dq 0
 section .text
 bits 64
 default rel
 
-global doublesum
-global scalarMultiply
-global getVectorSize
 
-doublesum:
-	addsd xmm0, xmm2
-	addsd xmm0, xmm1 ; xmm0 in 1st param, xmm1 in 2nd param, answer in xmm0
-	
-	ret
+global scalarMultiply
+global vectorAddition
+
 
 scalarMultiply:
-	movsd [vectorsize], xmm0 ; Move A to a variable
-	mov R12, rcx ; r12 will contain the pointer to our array
-	movsd xmm0, [r12] ; access the first value of the vector
-	mov rcx, r8 ; save the vector size into rcx as this will be our loop counter
-	MOV R15, 0 ; this acts as our index into the array.
+	; 1st parameter (vector size) is stored in RCX as our loop counter
+	; 2nd parameter (pointer to vector/array) is stored in RDX
+	; 3rd parameter (pointer to Z: the array that contains the results) is stored in R8
+	movsd [a], xmm3 ; 4th parameter (A) is stored in xmm2 and will be stored in a variable
+	mov r15, 0 ; r15 will act as our array indexer
 
-
-
-	L1:
-		movsd xmm0, [r12 + r15] ; access the current value of the array
-		MULSD xmm0, [vectorsize] ; multiply the first value of the vector by A
-		movsd [r12 + r15], xmm0 ; update the array accordingly.
+	MULLOOP:
+		movsd xmm0, [rdx + r15] ; access the current value of the array
+		MULSD xmm0, [a] ; multiply the value in the array by A
+		movsd [r8 + r15], xmm0 ; store the new value in Z
 		add r15, 8 ; increment the counter by 8 bytes (64-bits away)
-		LOOP L1
+		LOOP MULLOOP
 
-	FINIS: NOP
+	MULFINIS: NOP
 
 	ret
 
-getVectorSize:
-	mov rax, rcx
+vectorAddition:
+
+	; 1st parameter (vector size) is stored in RCX as our loop counter
+	mov r15, 0 ; r15 will act as our array indexer
+
+	ADDLOOP:
+		movsd xmm1, [rdx + r15] ; point to current value of 1st vector
+		movsd xmm2, [r8 + r15] ; point to current value of 2nd vector
+		addsd xmm1, xmm2 ; add both elements
+		movsd xmm0, xmm1 ; move the result into the return value
+
+		; store the result in 1st vector
+		movsd [rdx + r15], xmm0
+
+		add r15, 8 ; increment the index by 8 bytes (64 bits away)
+		LOOP ADDLOOP
+
+	ADDFINIS: NOP
+
 	ret
--- LBYARCH MP 2/main.c
+++ LBYARCH MP 2/main.c
@@ -6,21 +6,20 @@
 // Contains DAXPY operation for C
 #include "daxpy.h"
 
-#define N 3
+#define N 10
 
 // Import ASM functions
-extern double doublesum(double d1, double d2, double vectorSize);
-extern double scalarMultiply(double a, double vector[], long long int vectorSize);
-extern long long int getVectorSize(long long int vectorSize);
+extern void scalarMultiply(long long int vectorSize, double origVector[], double resultVector[], double a);
+extern void vectorAddition(long long int vectorSize, double vector1[], double vector2[]);
 
 // Declare Vectors as Arrays
 double X[N];
 double Y[N];
 double Z[N]; // Where Results for DAXPY is stored (For kernel C)
 
-// populate X with random values from 1 - 100.
-
-//extern long long int asmfunctionthatdoesprocess(X, Y);
+// double X[N] = {1.5, 2.021, 3.789};
+// double Y[N] = {11.7892314, 12.8745, 13.142};
+// double Z[N] = {0, 0, 0};
 
 
 static void fillArray(double array[], int size) {
@@ -56,19 +55,26 @@ int main(int argc, char* argv[]) {
 	// Execute algorithm for x86-64 Kernel
 	printf("\nx86-64 Run:\n");
 
-	long long int vectorSize = N;
-	printf("Vector Size is: %lli\n", getVectorSize(vectorSize));
-
 	// Point the current assembly to the X vector array (IDK WHY WE NEED TO DO THIS)
 	double d1 = *X; 
 
 	// Multiply each value in X by A
-	double result = scalarMultiply(a, X, N);
-	printf("RESULT: %f\n\n", result);
+	printf("\nMultiplying all values in X by A...\n");
+	scalarMultiply(N, X, Z, a);
 	displayVectorContents(X, N);
 
+	// Add Z (which contains A * X) and Y together
+	printf("\nAdding A * X and Y together...\n");
+	vectorAddition(N, Z, Y);
+
+	printf("\nFinal Results in Vector Z: \n");
+	displayVectorContents(Z, N);
+	
+
+
 	// Execute algorithm for C kernel
-	printf("\nC Kernel Run\n");
+
+	printf("\nC Kernel Run:\n\n");
 	t = clock(); // Clock start time
 	daxpy(a, X, Y, Z, N); // Perform DAXPY for C
 	t = clock() - t; // Clock end time
