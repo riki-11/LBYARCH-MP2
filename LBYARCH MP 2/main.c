006176c0172af3a10e40fbbf75dd82294a725386
PJLeonida <patrick_josh_c_leonida@dlsu.edu.ph>
2024-03-30 17:27:46 +0800

    Accuracy check and average time implemented

--- LBYARCH MP 2/main.c
+++ LBYARCH MP 2/main.c
@@ -6,7 +6,9 @@
 // Contains DAXPY operation for C
 #include "daxpy.h"
 
-#define N 2^30
+//#define N 67108864 // 2^26
+#define N 16777216 // 2^24
+//#define N 1048576  // 2^20
 
 
 double X[N];
@@ -25,25 +27,52 @@ static void fillArray(double array[], int size) {
 	}
 }
 
-int main(int argc, char* argv[]) {
-	srand(123);
-	float a = 2.0;
-	clock_t t;
+static void checkAccuracy(double Z[], double Z_1[], int size) {
+	int i, a_counter = 0; // Innaccuracy counter
 
+	for (i = 0; i < size; i++) {
+		if (Z[i] != Z_1[i]) {
+			a_counter++;
+		}
+	}
+	if (a_counter > 0)
+		printf("x86-64 kernel output is incorrect %d times", a_counter);
+	else
+		printf("86-64 kernel output is correct");
+}
+
+int main(int argc, char* argv[]) {
+	int i;
+	float a = 2.0;
+	double total_timeC = 0;
+	clock_t start, end;
+
+	srand(123); // Seed for reproducibility
 
 	// Populate X and Y
 	fillArray(X, N);
 	fillArray(Y, N);
 
-	t = clock(); // Clock start time
-	daxpy(a, X, Y, Z, N); // Perform DAXPY for C
-	t = clock() - t; // Clock end time
+	// Execute 30 times for testing
+	for (i = 0; i < 30; i++) {
 
-	double time = ((double)(t)) / CLOCKS_PER_SEC; // Calculate total time
+		start = clock();			// Clock start time
+		daxpy(a, X, Y, Z, N);		// Perform DAXPY for C
+		end = clock();				// Clock end time
+
+		/* PUT ASSEMBLY OPERATIONS*/
+
+
+		/* TOTAL TIME FOR BOTH */
+		total_timeC = total_timeC + ((double)(end - start)) / CLOCKS_PER_SEC; // Calculate total time in seconds
+	}
+
 
 	for (int i = 0; i < 10; i++) {
 		printf("%.2f | %.2f = %.2f\n", X[i], Y[i], Z[i]);
 	}
-	printf("Seconds: %f", time);
+
+
+	printf("Average time: %f", total_timeC/30.0);
 	return 0;
 }
